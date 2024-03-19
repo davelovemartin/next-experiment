2bf7902e29c17476c84b97cac68b82feebf14dbf
justan0therdave <justanotherdavemartin@gmail.com>
2024-03-19 09:24:19 +0000

    adding ts

---
+++ components/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Footer.module.css";
+
+export default function Footer() {
+  return (
+    <>
+      <footer className={styles.footer}>
+        <img
+          src="/logo-netlify.svg"
+          alt="Netlify Logo"
+          className={styles.logo}
+        />
+      </footer>
+    </>
+  );
+}
---
+++ components/Header.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+type Props = {
+  title: string;
+};
+
+export default function Header({ title }: Props) {
+  return <h1 className="title">{title}</h1>;
+}
