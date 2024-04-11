3fc81f04f2d27c684a2039feb4eadb2b1c01af4b
justan0therdave <justanotherdavemartin@gmail.com>
2024-04-11 15:38:16 +0100

    adding test and mocking styles

--- components/Footer/Footer.test.js
+++ components/Footer/Footer.test.js
@@ -1,1 +1,7 @@
-test("it works", () => {})
+import React from "react";
+import { render } from "@testing-library/react";
+import Footer from "./Footer";
+
+test("it renders", () => {
+  render(<Footer title="test" />);
+});
